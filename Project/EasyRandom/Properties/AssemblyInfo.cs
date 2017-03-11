3f0a3b175d59b3d098744bc16109072e57da6d01
tim <jan-brenner@gmx.de>
2017-03-11 13:13:32 +0100

    Updated assembly company and cleaned up code.

--- Project/EasyRandom/Properties/AssemblyInfo.cs
+++ Project/EasyRandom/Properties/AssemblyInfo.cs
@@ -4,21 +4,24 @@ using System.Runtime.InteropServices;
 // Allgemeine Informationen über eine Assembly werden über die folgenden 
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
+
 [assembly: AssemblyTitle("EasyRandom")]
 [assembly: AssemblyDescription("EasyRandom")]
 [assembly: AssemblyConfiguration("EasyRandom")]
 [assembly: AssemblyCompany("Haemmer Electronics")]
 [assembly: AssemblyProduct("EasyRandom")]
-[assembly: AssemblyCopyright("Copyright © Haemmer Electronics 2016")]
+[assembly: AssemblyCopyright("Copyright © Hämmer Electronics 2016")]
 [assembly: AssemblyTrademark("EasyRandom")]
 [assembly: AssemblyCulture("")]
 
 // Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
 // für COM-Komponenten.  Wenn Sie auf einen Typ in dieser Assembly von 
 // COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+
 [assembly: ComVisible(false)]
 
 // Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+
 [assembly: Guid("e5481c5f-7da9-4405-b551-9fbf2c13d96d")]
 
 // Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
@@ -31,5 +34,6 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
