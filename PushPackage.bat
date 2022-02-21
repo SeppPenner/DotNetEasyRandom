cc30b1b4a74330d7665141df641e7b7e3b721638
SeppPenner <HansM2013@gmx.de>
2022-02-21 16:37:18 +0100

    Updated build script.

--- PushPackage.bat
+++
@@ -1,3 +0,0 @@
-dotnet nuget push "src\DotNetEasyRandom\bin\Release\HaemmerElectronics.SeppPenner.DotNetEasyRandom.*.nupkg" -s "github" --skip-duplicate
-dotnet nuget push "src\DotNetEasyRandom\bin\Release\HaemmerElectronics.SeppPenner.DotNetEasyRandom.*.nupkg" -s "nuget.org" --skip-duplicate -k "%NUGET_API_KEY%"
-PAUSE
